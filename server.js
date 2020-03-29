1c11e0db80d5685e2752f6b1342f3f916a697c66
Doug Wilson <douglas.e.wilson@nasa.gov>
2020-03-29 10:40:26 -0400

    add dynamic GET route

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 
 
-
-
-
 const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
@@ -15,6 +12,15 @@ const mockUserData=[
               message: 'successfully got users. Nice!',
               users: mockUserData
          })
+    })
+
+    app.get('/users/:id',function(req,res){
+        console.log(req.params.id)
+        res.json({
+            success: true,
+            message: 'got one user',
+            user: req.params.id
+        })
     })
 
 app.listen(8000,function(){
